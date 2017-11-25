fa8ae3c864c11bf381d25888f3097f335d855954
jahav <havlicek.honza@gmail.com>
2017-11-25 21:39:45 +0100

    Add working implementation of Birthnumber with tests.

--- Identifiers.Tests/BirthNumberTest.cs
+++ Identifiers.Tests/BirthNumberTest.cs
@@ -28,5 +28,122 @@ namespace Identifiers.Tests
         {
             Assert.False(new BirthNumber("123456").HasStandardForm);
         }
+
+        [Theory]
+        [InlineData("09004264555", false)]
+        [InlineData("9004264555", true)]
+        public void NumberIsValidOnlyIfHasStandardForm(string birthNumber, bool shouldBeValid)
+        {
+            Assert.Equal(shouldBeValid, new BirthNumber(birthNumber).IsValid);
+        }
+
+        [Theory]
+        [InlineData("505504237", true)]
+        [InlineData("355105307", true)]
+        [InlineData("505500001", false)]
+        [InlineData("530004001", false)]
+        [InlineData("539904001", false)]
+        [InlineData("530599001", false)]
+        public void DatePartMustBeValidDateForValidNumber(string birthNumber, bool shouldBeValid)
+        {
+            Assert.Equal(shouldBeValid, new BirthNumber(birthNumber).IsValid);
+        }
+
+        [Theory]
+        [InlineData("505504237", 5)]
+        [InlineData("126224452", 12)]
+        [InlineData("355001001", null)]
+        [InlineData("455008001", null)]
+        [InlineData("125131001", 1)]
+        public void MonthPartHasAdded50ForFermales(string birthNumber, int? birthMonth)
+        {
+            Assert.Equal(birthMonth, new BirthNumber(birthNumber).Month);
+        }
+
+        [Theory]
+        [InlineData("032101001", null)]
+        [InlineData("037101001", null)]
+        [InlineData("0321010010", null)]
+        [InlineData("0371010010", null)]
+        [InlineData("0420010010", null)]
+        [InlineData("0421010010", 1)]
+        [InlineData("0429010010", 9)]
+        [InlineData("0432010010", 12)]
+        [InlineData("0433010010", null)]
+        [InlineData("0470010010", null)]
+        [InlineData("0471010010", 1)]
+        [InlineData("0479010010", 9)]
+        [InlineData("0482010010", 12)]
+        [InlineData("0483010010", null)]
+        public void MonthPartCanHave20AddedWhenAllocationExhausedAfterYear2003(string birthNumber, int? birthMonth)
+        {
+            Assert.Equal(birthMonth, new BirthNumber(birthNumber).Month);
+        }
+
+        [Theory]
+        [InlineData("540101001", 1854)]
+        [InlineData("990101001", 1899)]
+        [InlineData("000101001", 1900)]
+        [InlineData("530101001", 1953)]
+        [InlineData("5401010010", 1954)]
+        [InlineData("9901010010", 1999)]
+        [InlineData("0001010010", 2000)]
+        [InlineData("1701010010", 2017)]
+        [InlineData("5301010010", 2053)]
+        public void YearIsAccuratelyDetermined(string birthNumber, int expectedYear)
+        {
+            Assert.Equal(expectedYear, new BirthNumber(birthNumber).Year);
+        }
+
+        public void YearIsNullIfNumberIsNotInStandardForm()
+        {
+            Assert.Null(new BirthNumber("123456").Year);
+        }
+
+        [Theory]
+        [InlineData("5401010010", 0)]
+        [InlineData("6503192641", 1)]
+        [InlineData("9559121902", 2)]
+        [InlineData("0151194483", 3)]
+        [InlineData("7953229944", 4)]
+        [InlineData("9811085515", 5)]
+        [InlineData("5552242696", 6)]
+        [InlineData("0004266207", 7)]
+        [InlineData("6759141488", 8)]
+        [InlineData("5455285869", 9)]
+        [InlineData("7801233540", 0)]
+        public void ExpectedCheckDigitIsSumModulo11(string birthNumber, int expectedCheckDigit)
+        {
+            Assert.Equal(expectedCheckDigit, new BirthNumber(birthNumber).ExpectedCheckDigit);
+        }
+
+        [Theory]
+        [InlineData("530101001")]
+        [InlineData("700101001")]
+        public void ExpectedCheckDigitIsNullBefore1954(string birthNumber)
+        {
+            Assert.Null(new BirthNumber(birthNumber).ExpectedCheckDigit);
+        }
+
+        [Theory]
+        [InlineData("0000")]
+        [InlineData("0xDEADBEEF")]
+        public void ExpectedCheckDigitIsNullInNumbersWithoutStandardForm(string birthNumber)
+        {
+            Assert.Null(new BirthNumber(birthNumber).ExpectedCheckDigit);
+        }
+
+        [Theory]
+        [InlineData("0001010008", false)]
+        [InlineData("0001010009", true)]
+        [InlineData("0001010010", true)]
+        [InlineData("0001010011", false)]
+        [InlineData("5412242561", true)]
+        [InlineData("7552318510", false)]
+        
+        public void NumberAfter1954IsValidOnlyIfItIsHasStandardFormAndValidDatePartAndCheckDigit(string birthNumber, bool shouldBeValid)
+        {
+            Assert.Equal(shouldBeValid, new BirthNumber(birthNumber).IsValid);
+        }
     }
 }
--- Identifiers/BirthNumber.cs
+++ Identifiers/BirthNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -15,25 +16,195 @@ namespace Identifiers
     ///  <li>check digit (only after 1954)</li>
     /// </ul>
     /// </summary>
+    /// <see cref="http://www.mvcr.cz/clanek/overovani-rodneho-cisla-331794.aspx"/>
     public class BirthNumber
     {
+        private const int centuryYear1954 = 54;
+        private const int yearDigit0 = 0;
+        private const int yearDigit1 = 1;
+        private const int monthDigit0 = 2;
+        private const int monthDigit1 = 3;
+        private const int dayDigit0 = 4;
+        private const int dayDigit1 = 5;
+        private const int sequenceDigit0 = 6;
+        private const int sequenceDigit1 = 7;
+        private const int sequenceDigit2 = 8;
+        private const int checkDigit = 9;
+
+        /// <summary>
+        /// Birth numbers for women have added 50 for their month part, e.g. 455508/001 means a first woman born at 8 May 1945.
+        /// <see cref="https://www.zakonyprolidi.cz/cs/2000-133/">Law number. 133/2000 § 13 (5)</see>
+        /// </summary>
+        private const int womanMonthShift = 50;
+
+
+        /// <summary>
+        /// When all sequential numbers for a day are assigned, it is possible to utilize another space by adding 20 to the month part.
+        /// <see cref="https://www.zakonyprolidi.cz/cs/2000-133/">Law number. 133/2000 § 13 (5)</see>
+        /// </summary>
+        private const int exhaustMonthShift = 20;
+
         /// <summary>
         /// Every birth number after 1.1.1954 has a check digit, so it is 10 digits.
         /// </summary>
         /// <remarks><c>\d</c> character class includes other digits from other character set, so it is not used.</remarks>
-        private static Regex standardForm = new Regex("^[0-9]{9,10}$");
+        private readonly static Regex standardForm = new Regex("^[0-9]{9,10}$");
 
-        private string raw;
+        private readonly string raw;
+
+        /// <summary>
+        /// Digits of the birth number. Only set if birth number is in standard form.
+        /// </summary>
+        private readonly int[] digits;
+        private readonly int year;
+        private readonly int month;
+        private readonly int day;
+        private readonly int expectedCheckDigit;
+        private readonly bool isDatePartValid;
 
         /// <summary>
         /// Create a new instance of a <see cref="BirthNumber"/>.
         /// </summary>
         /// <param name="input">A string that will be used as a identification number. Can be null.</param>
-        public BirthNumber(string input) {
+        public BirthNumber(string input)
+        {
             raw = input;
             HasStandardForm = input != null && standardForm.IsMatch(input);
+            if (!HasStandardForm)
+            {
+                return;
+            }
+
+            digits = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                digits[i] = input[i] - '0';
+            }
+
+            year = CalculateYear();
+            month = CalculateMonth();
+            day = digits[dayDigit0] * 10 + digits[dayDigit1];
+            isDatePartValid = CheckDateValidity();
+
+            var number = 0;
+            for (int i = yearDigit0; i <= sequenceDigit2; i++)
+            {
+                number = number * 10 + digits[i];
+            }
+
+            var modulo = number % 11;
+            expectedCheckDigit = modulo == 10 ? 0 : modulo;
         }
 
+        /// <summary>
+        /// Does the input have a standard form?
+        /// </summary>
         public bool HasStandardForm { get; }
+
+        /// <summary>
+        /// Is the birth number standard and valid?
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                if (HasStandardForm && isDatePartValid)
+                {
+                    if (IsAfter1954)
+                    {
+                        return expectedCheckDigit == digits[checkDigit];
+                    }
+
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Return a year of birth, if the number has a standard form.
+        /// </summary>
+        public int? Year => HasStandardForm ? year : (int?)null;
+
+        /// <summary>
+        /// Return a month of birth, if the number has a standard form and month is within a valid range.
+        /// </summary>
+        public int? Month => (HasStandardForm && month >= 1 && month <= 12) ? month : (int?)null;
+
+        /// <summary>
+        /// Return a day of birth, if the number has a standard form.
+        /// </summary>
+        public int? Day => HasStandardForm ? day : (int?)null;
+
+        /// <summary>
+        /// Get expected check digit, if the birth number has a standard form and is after year 1954.
+        /// </summary>
+        public int? ExpectedCheckDigit => HasStandardForm && IsAfter1954 ? expectedCheckDigit : (int?)null;
+
+        private bool IsAfter1954 => raw.Length == 10;
+
+        public int CalculateYear()
+        {
+            var yearInCentury = digits[yearDigit0] * 10 + digits[yearDigit1];
+            int year = yearInCentury;
+            if (IsAfter1954)
+            {
+                if (yearInCentury < centuryYear1954)
+                {
+                    year += 2000;
+                }
+                else
+                {
+                    year += 1900;
+                }
+            }
+            else
+            {
+                if (yearInCentury < centuryYear1954)
+                {
+                    year += 1900;
+                }
+                else
+                {
+                    year += 1800;
+                }
+            }
+
+            return year;
+        }
+
+        private int CalculateMonth()
+        {
+            var month = digits[monthDigit0] * 10 + digits[monthDigit1];
+            var isFemale = month > womanMonthShift;
+            if (isFemale)
+            {
+                month -= 50;
+            }
+
+            // law for exhaustion entered force at 2004
+            var isExhaused = year > 2003 && month > exhaustMonthShift;
+            if (isExhaused)
+            {
+                month -= exhaustMonthShift;
+            }
+
+            return month;
+        }
+
+        private bool CheckDateValidity()
+        {
+            if (month < 1 || month > 12 || day < 1)
+            {
+                return false;
+            }
+
+            if (day > DateTime.DaysInMonth(year, month))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
