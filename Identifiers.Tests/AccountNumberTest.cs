93ae0a93a118538478de407d7244af431425b9f9
jahav <havlicek.honza@gmail.com>
2017-11-26 01:29:16 +0100

    Add czech account number and tests for it.

---
+++ Identifiers.Tests/AccountNumberTest.cs
@@ -0,0 +1,117 @@
+﻿using Xunit;
+
+namespace Identifiers.Tests
+{
+    public class AccountNumberTest
+    {
+        [Fact]
+        public void NullDoesntHaveStandardForm()
+        {
+            Assert.False(new AccountNumber(null).HasStandardFormat);
+        }
+
+        [Theory]
+        [InlineData("0012/0300", 12, "0300")]
+        [InlineData("1234567890/6200", 1234567890, "6200")]
+        [InlineData("0000/6200", 0, "6200")]
+        public void CanAcceptAccountWithoutPrefix(string accountNumber, int? expectedNumber, string expectedBankCode)
+        {
+            var account = new AccountNumber(accountNumber);
+            Assert.True(account.HasStandardFormat);
+            Assert.Equal(expectedNumber, account.Number);
+            Assert.Equal(expectedBankCode, account.BankCode);
+        }
+
+        [Theory]
+        [InlineData("17-0012/0300", 17, 12, "0300")]
+        [InlineData("000-1234567890/6200", 0, 1234567890, "6200")]
+        public void CanAcceptAccountWithPrefix(string accountNumber, int? expectedPrefix, int? expectedNumber, string expectedBankCode)
+        {
+            var account = new AccountNumber(accountNumber);
+            Assert.True(account.HasStandardFormat);
+            Assert.Equal(expectedPrefix, account.Prefix);
+            Assert.Equal(expectedNumber, account.Number);
+            Assert.Equal(expectedBankCode, account.BankCode);
+        }
+
+        [Theory]
+        [InlineData("1234567-12/0100")]
+        [InlineData("abcdef-12/0100")]
+        public void PrefixMustBe0To6DigitsToBeInStandardForm(string invalidInput)
+        {
+            var invalidAccount = new AccountNumber(invalidInput);
+            Assert.False(invalidAccount.HasStandardFormat);
+            Assert.Null(invalidAccount.Prefix);
+            Assert.Null(invalidAccount.Number);
+            Assert.Null(invalidAccount.BankCode);
+        }
+
+        [Theory]
+        [InlineData("1/0100")]
+        [InlineData("12345678901/0100")]
+        [InlineData("abc/0100")]
+        [InlineData("a2/0100")]
+        public void AccountNumberMustBe2To10DigitsToBeInStandardForm(string invalidInput)
+        {
+            var invalidAccount = new AccountNumber(invalidInput);
+            Assert.False(invalidAccount.HasStandardFormat);
+            Assert.Null(invalidAccount.Prefix);
+            Assert.Null(invalidAccount.Number);
+            Assert.Null(invalidAccount.BankCode);
+        }
+
+        [Theory]
+        [InlineData("12/123")]
+        [InlineData("12/12345")]
+        [InlineData("12/abcd")]
+        [InlineData("12/A100")]
+        public void BankCode4DigitsToBeInStandardForm(string invalidInput)
+        {
+            var invalidAccount = new AccountNumber(invalidInput);
+            Assert.False(invalidAccount.HasStandardFormat);
+            Assert.Null(invalidAccount.Prefix);
+            Assert.Null(invalidAccount.Number);
+            Assert.Null(invalidAccount.BankCode);
+        }
+
+        [Theory]
+        [InlineData("00/0100", false)]
+        [InlineData("10/0100", false)]
+        [InlineData("19/0100", true)]
+        [InlineData("9000000001/0100", true)]
+        [InlineData("5000000000/0100", false)]
+        [InlineData("000400000/0100", false)]
+        public void NumberMustHaveAtLeast2NonZeroDigitsToBeValid(string account, bool isValid)
+        {
+            Assert.Equal(isValid, new AccountNumber(account).IsValid);
+        }
+
+        [Theory]
+        [InlineData("19-19/0100", 11, true)]
+        [InlineData("742418-19/0100", 132, true)]
+        [InlineData("00483-19/0000", 35, false)]
+        [InlineData("575427-19/0000", 152, false)]
+        [InlineData("58509-19/0000", 118, false)]
+        [InlineData("73-19/0000", 17, false)]
+        public void PrefixChecksumMustBeFullyDivisibleBy11ToBeValid(string account, int prefixChecksum, bool isValid)
+        {
+            var accountNumber = new AccountNumber(account);
+            Assert.Equal(accountNumber.PrefixChecksum % 11 == 0, isValid);
+            Assert.Equal(prefixChecksum, accountNumber.PrefixChecksum);
+            Assert.Equal(isValid, accountNumber.IsValid);
+        }
+
+        [Theory]
+        [InlineData("765178/0100", 166, false)]
+        [InlineData("37/0100", 13, false)]
+        [InlineData("78/0100", 22, true)]
+        [InlineData("9999999999/0100", 495, true)]
+        public void NumberChecksumMustBeFullyDivisibleBy11ToBeValid(string account, int numberChecksum, bool isValid)
+        {
+            var accountNumber = new AccountNumber(account);
+            Assert.Equal(accountNumber.NumberChecksum % 11 == 0, isValid);
+            Assert.Equal(numberChecksum, accountNumber.NumberChecksum);
+            Assert.Equal(isValid, accountNumber.IsValid);
+        }
+    }
+}
---
+++ Identifiers/AccountNumber.cs
@@ -0,0 +1,135 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Identifiers
+{
+    /// <summary>
+    /// An account number used in Czech Republic, it consists from three parts:
+    /// <ul>
+    /// <li>Prefix (optional)</li>
+    /// <li>Account number</li>
+    /// <li>Bank code</li>
+    /// </ul>
+    /// and generally between prefix and account number is a dash, while between account number and bank code is a slash.
+    /// <example>
+    /// 19-123457/0710
+    /// </example>
+    /// </summary>
+    /// <see cref="https://www.cnb.cz/miranda2/export/sites/www.cnb.cz/cs/platebni_styk/pravni_predpisy/download/vyhl_169_2011.pdf">Decree 169/2011</see>
+    /// <see cref="https://www.cnb.cz/cs/platebni_styk/iban/iban_napoveda.html"/>
+    public class AccountNumber : IIdentifier
+    {
+        /// <summary>
+        /// Weights of the digits, from the rightmost to the leftmost (=index 0 is the rightmost one, index 9 is leftmost one).
+        /// </summary>
+        private static int[] weights = new int[] { 1, 2, 4, 8, 5, 10, 9, 7, 3, 6 };
+
+        /// <summary>
+        /// Accoring to the CNB decree no.169/2011, the prefix and account number should be 
+        /// "clearly separated", so it could possibly be something else than dash, but dash is used by everyone.
+        /// Use parser for converions.
+        /// </summary>
+        private static Regex standardForm = new Regex("^(([0-9]{1,6})-|-?)([0-9]{2,10})/([0-9]{4}$)");
+
+        private readonly string input;
+
+        public AccountNumber(string accountNumber)
+        {
+            input = accountNumber;
+            if (accountNumber == null)
+            {
+                HasStandardFormat = false;
+                return;
+            }
+
+            Match match = standardForm.Match(accountNumber);
+            if (!match.Success)
+            {
+                HasStandardFormat = false;
+                return;
+            }
+
+            HasStandardFormat = true;
+            bool prefixFound = match.Groups[2].Captures.Count == 1;
+            if (prefixFound)
+            {
+                Prefix = long.Parse(match.Groups[2].Captures[0].Value, NumberStyles.None, CultureInfo.InvariantCulture);
+            }
+
+            Number = long.Parse(match.Groups[3].Captures[0].Value, NumberStyles.None, CultureInfo.InvariantCulture);
+            BankCode = match.Groups[4].Captures[0].Value;
+        }
+
+        public bool HasStandardFormat { get; }
+
+        public bool IsValid => HasStandardFormat && PrefixChecksum % 11 == 0 && NumberChecksum % 11 == 0 && CalculateNonDigitCount(Number) >= 2;
+
+        /// <summary>
+        /// Prefix part of the the account, if the number has a standard form.
+        /// </summary>
+        public long? Prefix { get; }
+
+        /// <summary>
+        /// The account number, if the number has a standard form. 
+        /// </summary>
+        public long? Number { get; }
+
+        /// <summary>
+        /// Code of the bank where the account is, if the number has a standard form.
+        /// </summary>
+        public string BankCode { get; }
+
+        /// <summary>
+        /// Get a checksum for a prefix, if the number has a standard form.
+        /// </summary>
+        public int? PrefixChecksum => HasStandardFormat ? CheckSum(Prefix ?? 0) : (int?)null;
+
+        /// <summary>
+        /// Get a checksum for a prefix, if the number has a standard form.
+        /// </summary>
+        public int? NumberChecksum => HasStandardFormat ? CheckSum(Number ?? 0) : (int?)null;
+
+        /// <summary>
+        /// Get a number of digits that are not zero.
+        /// </summary>
+        /// <param name="part">Tested part</param>
+        /// <returns>Number of digits in the <paramref name="part"/> that are not zero.</returns>
+        private int CalculateNonDigitCount(long? part)
+        {
+            if (part == null) return 0;
+
+            int count = 0;
+            var value = part.Value;
+            while (value > 0)
+            {
+                var digit = value % 10;
+                if (digit != 0)
+                {
+                    count++;
+                }
+
+                value = value / 10;
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Calculate a checksum of a part of the account.
+        /// </summary>
+        /// <param name="part">Part of the account, either prefix or account number.</param>
+        /// <returns>Calculated checksum.</returns>
+        private int CheckSum(long part)
+        {
+            var checksum = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                int digit = (int)(part % 10);
+                checksum += digit * weights[i];
+                part = part / 10;
+            }
+
+            return checksum;
+        }
+    }
+}
