92717ad35897a9b9892419fad298a42073c21216
jahav <havlicek.honza@gmail.com>
2017-12-05 01:03:38 +0100

    Improve documentation of IPattern.

--- Identifiers/IPattern.cs
+++ Identifiers/IPattern.cs
@@ -1,28 +1,31 @@
-﻿using System.Text;
-
-namespace Identifiers.Czech
+﻿namespace Identifiers.Czech
 {
     /// <summary>
-    /// A pattern of a string, used to transform a value from and to the strin with a certain pattern..
+    /// A string pattern of a type <see cref="TValue"/>, used to 
+    /// 
+    /// * parse a string and create value from the string.
+    /// * create a string from the value.
+    /// 
+    /// The implementation of the interface ensure that both parsing and formatting are using same string pattern, although parsing can be more generous with accepted input.
     /// </summary>
-    /// <typeparam name="T">Type of value.</typeparam>
+    /// <typeparam name="TValue">Type of parsed/formatted value.</typeparam>
     /// <see cref="IFormattable"/>.
-    public interface IPattern<T>
+    public interface IPattern<TValue>
     {
         /// <summary>
-        /// Format the <paramref name="value"/> according to the pattern.
+        /// Format the <paramref name="value"/> to a string according to the pattern.
         /// </summary>
         /// <param name="value">Value to format.</param>
         /// <returns>Formatted string.</returns>
-        string Format(T value);
+        string Format(TValue value);
 
         /// <summary>
-        /// Parse a text into the value. It doesn't thow exceptions, if an error 
+        /// Parse a string into the value. It doesn't thow exceptions, if an error 
         /// happens during parsing, return unsuccessfull <see cref="ParseResult{T}"/>.
         /// </summary>
         /// <param name="text">Text to parse.</param>
         /// <returns>Parsing result.</returns>
         /// <exception cref="ArgumentNotNull">If <paramref name="text"/> is <c>null</c>.</exception>
-        ParseResult<T> Parse(string text);
+        ParseResult<TValue> Parse(string text);
     }
 }
